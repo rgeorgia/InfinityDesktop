603fe1daaf11748d345e636ed55e7a8011b94bf4
Ron Georgia <netverbs@gmail.com>
2020-07-30 10:27:42 -0400

    Added config/fish

---
+++ dot-config/fish/config.fish
@@ -0,0 +1,4 @@
+# Set the search path for programs.
+set -x PATH $HOME/bin /bin /sbin /usr/bin /usr/sbin /usr/X11R7/bin /usr/pkg/bin
+set -gx PATH $PATH /usr/pkg/sbin /usr/games /usr/local/bin /usr/local/sbin
+set -g PATH $HOME/.cargo/bin $PATH
