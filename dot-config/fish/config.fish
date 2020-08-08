088736e91016547b2202633de7c77521cf7aa82a
Ron Georgia <netverbs@gmail.com>
2020-08-08 05:06:45 -0400

    Updated config.fish to include man page path

--- dot-config/fish/config.fish
+++ dot-config/fish/config.fish
@@ -1,4 +1,5 @@
 # Set the search path for programs.
-set -x PATH $HOME/bin /bin /sbin /usr/bin /usr/sbin /usr/X11R7/bin /usr/pkg/bin
+set -gx PATH $HOME/bin /bin /sbin /usr/bin /usr/sbin /usr/X11R7/bin /usr/pkg/bin
 set -gx PATH $PATH /usr/pkg/sbin /usr/games /usr/local/bin /usr/local/sbin
-set -g PATH $HOME/.cargo/bin $PATH
+set -x PATH $HOME/.cargo/bin $PATH
+set -g MANPATH /usr/share/man /usr/X11R7/man
