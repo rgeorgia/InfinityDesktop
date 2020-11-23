8ee4e2dac3d6a42a1c42f2268ac0fcb368a7b164
Ron Georgia <rong1611@gmail.com>
2020-11-23 15:52:01 -0500

    Update config.fish

--- dot-config/fish/config.fish
+++ dot-config/fish/config.fish
@@ -1,5 +1,6 @@
 # Set the search path for programs.
 set -gx PATH $HOME/bin /bin /sbin /usr/bin /usr/sbin /usr/X11R7/bin /usr/pkg/bin
 set -gx PATH $PATH /usr/pkg/sbin /usr/games /usr/local/bin /usr/local/sbin
-set -x PATH $HOME/.cargo/bin $PATH
-set -g MANPATH /usr/share/man /usr/X11R7/man
+set -gx PATH $HOME/.cargo/bin $PATH
+set -gx MANPATH /usr/share/man /usr/X11R7/man /usr/pkg/man
+
