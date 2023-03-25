fab988ba068140850585c9d27a50ef9aa0560688
Ron Georgia <rong1611@gmail.com>
2023-03-25 16:01:40 -0400

    Update cprcd and added make_home

--- desktops/bin/make_home.sh
+++ desktops/bin/make_home.sh
@@ -1,1 +1,5 @@
 mkdir -p $HOME/Document $HOME/Downloads $HOME/Templates $HOME/Videos $HOME/Pictures/backgrounds $HOME/Nowhere
+mkdir -p $HOME/.config/fish
+mkdir -p $HOME/.config/sakura
+mkdir -p $HOME/.fluxbox
+cp -r ../fluxbox/* $HOME/.fluxbox/.
