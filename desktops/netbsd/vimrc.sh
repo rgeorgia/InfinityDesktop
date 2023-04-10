0fb24b52a06dfe2d13bd737aa478f10ce9e1f79d
Ron Georgia <rong1611@gmail.com>
2023-04-10 09:35:56 -0400

    Updated menu for openbox

---
+++ desktops/netbsd/vimrc.sh
@@ -0,0 +1,6 @@
+pkgin in gh
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+git clone https://github.com/fatih/vim-go.git ~/.vim/pack/plugins/start/vim-go
+git clone https://github.com/fatih/vim-go.git ~/.vim/bundle/vim-go
+git clone https://github.com/rust-lang/rust.vim ~/.vim/pack/plugins/start/rust.vim
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
