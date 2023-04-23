fa1df0440865015595e2f2ee747236575dc71181
Ron Georgia <rong1611@gmail.com>
2023-04-23 11:27:27 -0400

    Updated make_home.sh to create openbox .config

--- desktops/bin/make_home.sh
+++ desktops/bin/make_home.sh
@@ -2,4 +2,6 @@ mkdir -p $HOME/Document $HOME/Downloads $HOME/Templates $HOME/Videos $HOME/Pictu
 mkdir -p $HOME/.config/fish
 mkdir -p $HOME/.config/sakura
 mkdir -p $HOME/.fluxbox
+mkdir -p $HOME/.config/openbox
 cp -r ../fluxbox/* $HOME/.fluxbox/.
+cp -r ../openbox/* $HOME/.config/openbox/.
