35e314789667550db2dd30606ecf3fe8cb1bffc0
Ron Georgia <rong1611@gmail.com>
2023-10-15 11:02:29 -0400

    Updated pull_repos.sh

--- desktops/bin/pull_repos.sh
+++ desktops/bin/pull_repos.sh
@@ -1,16 +1,23 @@
 URL=git@github.com:rgeorgia
-git clone $URL/biblenotes.git $HOME/workspace/hugo_projects/biblenotes
-git clone $URL/ronotes.git $HOME/workspace/hugo_projects/ronotes
-git clone $URL/prayerlist.git $HOME/workspace/hugo_projects/prayerlist
-git clone $URL/djkodiak.git $HOME/workspace/python_projects/djkodiak
-git clone $URL/nbsysrc.git $HOME/workspace/python_projects/nbsysrc
-git clone $URL/infinity_build.git $HOME/workspace/python_projects/infinity_build
-git clone $URL/iocage.git $HOME/workspace/python_projects/iocage
-git clone $URL/Oxidize-the-boring-stuff.git $HOME/workspace/rust_projects/Oxidize-the-boring-stuff
-git clone $URL/nbsysrc.rs.git $HOME/workspace/rust_projects/nbsysrc
-git clone $URL/fvwm-menu.git $HOME/workspace/rust_projects/fvwm-menu
-git clone $URL/nscat-rs.git $HOME/workspace/rust_projects/nscat-rs
-git clone $URL/morris_code.git $HOME/workspace/rust_projects/morris_code
-git clone $URL/Rust-Pi-Projects.git $HOME/workspace/rust_projects/Rust-Pi-Projects
+PYTHON=python_projects
+GO=go_projects
+HUGO=hugo_projects
+RUST=rust_projects
+git clone $URL/biblenotes.git $HOME/workspace/$HUGO/biblenotes
+git clone $URL/ronotes.git $HOME/workspace/$HUGO/ronotes
+git clone $URL/prayerlist.git $HOME/workspace/$HUGO/prayerlist
+git clone $URL/djkodiak.git $HOME/workspace/$PYTHON/djkodiak
+git clone $URL/nbsysrc.git $HOME/workspace/$PYTHON/nbsysrc
+git clone $URL/infinity_build.git $HOME/workspace/$PYTHON/infinity_build
+git clone $URL/iocage.git $HOME/workspace/$PYTHON/iocage
+git clone $URL/Oxidize-the-boring-stuff.git $HOME/workspace/$RUST/Oxidize-the-boring-stuff
+git clone $URL/nbsysrc.rs.git $HOME/workspace/$RUST/nbsysrc
+git clone $URL/fvwm-menu.git $HOME/workspace/$RUST/fvwm-menu
+git clone $URL/nscat-rs.git $HOME/workspace/$RUST/nscat-rs
+git clone $URL/morris_code.git $HOME/workspace/$RUST/morris_code
+git clone $URL/Rust-Pi-Projects.git $HOME/workspace/$RUST/Rust-Pi-Projects
 git clone $URL/netsysrc.git $HOME/workspace/go_projects/netsysrc
 git clone $URL/nscat.git $HOME/workspace/go_projects/nscat
+git clone $URL/iss_location.git $HOME/workspace/$RUST/iss_location
+git clone $URL/team-51-loki-6051ea9d.git $HOME/workspace/team-51-loki-6051ea9d
+git clone $URL/ASD-200-CMP.git $HOME/workspace/ASD-200-CMP.git
