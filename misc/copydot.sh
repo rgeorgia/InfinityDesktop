f290152884fa6cf124d4912d06adf6483dda7d2b
Ron Georgia <rong1611@gmail.com>
2023-11-12 16:37:00 -0500

    WIP

--- misc/copydot.sh
+++ misc/copydot.sh
@@ -1,8 +1,5 @@
 cp dot-Xresources $HOME/.Xresources
-cp dot-ctwmrc $HOME/.ctwmrc
 cp dot-gitconfig $HOME/.gitconfig
-cp dot-setdual.sh $HOME/.setdual.sh
-cp dot-tcshrc $HOME/.tcshrc
 cp dot-vimrc $HOME/.vimrc
 cp dot-xinitrc $HOME/.xinitrc
 cp dot-xprofile $HOME/.xprofile
