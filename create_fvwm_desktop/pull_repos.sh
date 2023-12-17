caaa84441db50c07f837b3de8f78698426dc03c2
Ron Georgia <rong1611@gmail.com>
2023-12-16 20:01:03 -0500

    WIP

--- create_fvwm_desktop/pull_repos.sh
+++ create_fvwm_desktop/pull_repos.sh
@@ -5,11 +5,14 @@ GO=go_projects
 HUGO=hugo_projects
 RUST=rust_projects
 git clone $URL/biblenotes.git $HOME/workspace/$HUGO/biblenotes
-git clone $URL/ronotes.git $HOME/workspace/$HUGO/ronotes
 git clone $URL/prayerlist.git $HOME/workspace/$HUGO/prayerlist
-git clone $URL/nbsysrc.git $HOME/workspace/$PYTHON/nbsysrc
+git clone $URL/journals.git $HOME/workspace/$HUGO/journals
 git clone $URL/nbsysrc.rs.git $HOME/workspace/$RUST/nbsysrc
-git clone $URL/fvwm-menu.git $HOME/workspace/$RUST/fvwm-menu
+git clone $URL/iss_location.git $HOME/workspace/$RUST/iss_location
+git clone $URL/fvwm-menu.rs.git $HOME/workspace/$RUST/fvwm-menu
 git clone $URL/nscat-rs.git $HOME/workspace/$RUST/nscat-rs
-git clone $URL/nbsysrc.go.git $HOME/workspace/go_projects/nbsysrc.go
-git clone $URL/nscat.git $HOME/workspace/go_projects/nscat
+git clone $URL/Oxidize-the-boring-stuff.git $HOME/workspace/$RUST/Oxidize-the-boring-stuff
+git clone $URL/nbsysrc.go.git $HOME/workspace/$GO/nbsysrc.go
+git clone $URL/nscat.git $HOME/workspace/$GO/nscat
+git clone $URL/stanque.git $HOME/workspace/$GO/stanque
+git clone $URL/fvwm-menu.git $HOME/workspace/$GO/fvwm-menu
