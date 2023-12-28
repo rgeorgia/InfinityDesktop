0ffca536e4a0e92030c6a47d210187aa981fe798
Ron Georgia <rong1611@gmail.com>
2023-12-27 22:31:15 -0500

    WIP

---
+++ create_fvwm_desktop/setup_home.py
@@ -0,0 +1,37 @@
+#!/usr/pkg/bin/python3.11
+
+# Makes home
+# copy dot files
+# Make Home
+# Make .config
+# change shell to fish
+# Make .fvwm
+
+"""
+# Makes home
+# copy dot files
+cp dot-files/dot-Xresources $HOME/.Xresources
+cp dot-files/dot-gitconfig $HOME/.gitconfig
+cp dot-files/dot-vimrc $HOME/.vimrc
+cp dot-files/dot-tcshrc $HOME/.tcshrc
+cp dot-files/dot-xinitrc $HOME/.xinitrc
+cp dot-files/dot-xprofile $HOME/.xprofile
+ln $HOME/.xinitrc $HOME/.xsession
+
+# Make Home
+mkdir -p $HOME/Document $HOME/Downloads $HOME/Templates $HOME/Videos $HOME/Pictures/backgrounds $HOME/Nowhere
+
+# Make .config
+mkdir -p $HOME/.config/fish
+mkdir -p $HOME/.config/sakura
+mkdir -p $HOME/.config/fvwm3
+cp -r dot-config/fish/* $HOME/.config/fish/.
+cp -r dot-config/sakura/* $HOME/.config/sakura/.
+cp -r dot-config/fvwm3/* $HOME/.config/fvwm3/.
+
+chsh -s /usr/pkg/bin/fish
+
+# Make .fvwm
+mkdir -p $HOME/.fvwm
+cp -r dot-fvwm/fvwm/* $HOME/.fvwm
+"""
