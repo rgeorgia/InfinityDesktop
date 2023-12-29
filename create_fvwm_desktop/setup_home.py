06bbb402924cff835295febc452bac98ff5e63fc
Ron Georgia <rong1611@gmail.com>
2023-12-28 22:45:59 -0500

    WIP

--- create_fvwm_desktop/setup_home.py
+++ create_fvwm_desktop/setup_home.py
@@ -1,9 +1,6 @@
 #!/usr/pkg/bin/python3.11
 
 """
-# Make Home
-mkdir -p $HOME/Document $HOME/Downloads $HOME/Templates $HOME/Videos $HOME/Pictures/backgrounds $HOME/Nowhere
-
 # Make .config
 mkdir -p $HOME/.config/fish
 mkdir -p $HOME/.config/sakura
@@ -26,7 +23,9 @@ class SetupHome:
     def __init__(self):
         self.dot_file_location = Path(".").joinpath("dot-files")
         self.dot_fvwm_location = Path(".").joinpath("dot-fvwm")
+        self.dot_config_location = Path(".").joinpath("dot-config")
         self.dirs_to_create = ['Documents', 'Downloads', 'Templates', 'Videos', 'Pictures/backgrounds', 'Nowhere', ]
+        self.dot_config_dirs = ['fish', 'sakura', 'fvwm3']
 
     def copy_dot_files(self):
         for item in self.dot_file_location.iterdir():
@@ -41,13 +40,25 @@ class SetupHome:
             new_dir.mkdir(parents=True, exist_ok=True)
 
     def make_dot_config(self):
-        pass
+        """
+        cp -r dot-config/fish/* $HOME/.config/fish/.
+        cp -r dot-config/sakura/* $HOME/.config/sakura/.
+        cp -r dot-config/fvwm3/* $HOME/.config/fvwm3/.
+        :return:
+        """
+        for item in self.dot_config_dirs:
+            new_dir = Path.home().joinpath(".config").joinpath(item)
+            new_dir.mkdir(parents=True, exist_ok=True)
 
     def make_dot_fvwm(self):
         pass
 
-    def symlink_initrc(self):
-        pass
+    @staticmethod
+    def symlink_initrc():
+        # create symlink from .xinitrc to .xsession
+        src = Path.home().joinpath(".xinitrc")
+        target = Path.home().joinpath(".xsession")
+        src.symlink_to(str(target))
 
 
 class UserConfig:
