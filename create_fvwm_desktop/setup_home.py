1494885de9181fb1ea00076e52077a2448cd6ff0
Ron Georgia <rong1611@gmail.com>
2023-12-28 22:49:29 -0500

    WIP

--- create_fvwm_desktop/setup_home.py
+++ create_fvwm_desktop/setup_home.py
@@ -1,13 +1,6 @@
 #!/usr/pkg/bin/python3.11
 
 """
-# Make .config
-mkdir -p $HOME/.config/fish
-mkdir -p $HOME/.config/sakura
-mkdir -p $HOME/.config/fvwm3
-cp -r dot-config/fish/* $HOME/.config/fish/.
-cp -r dot-config/sakura/* $HOME/.config/sakura/.
-cp -r dot-config/fvwm3/* $HOME/.config/fvwm3/.
 
 chsh -s /usr/pkg/bin/fish
 
@@ -23,9 +16,7 @@ class SetupHome:
     def __init__(self):
         self.dot_file_location = Path(".").joinpath("dot-files")
         self.dot_fvwm_location = Path(".").joinpath("dot-fvwm")
-        self.dot_config_location = Path(".").joinpath("dot-config")
         self.dirs_to_create = ['Documents', 'Downloads', 'Templates', 'Videos', 'Pictures/backgrounds', 'Nowhere', ]
-        self.dot_config_dirs = ['fish', 'sakura', 'fvwm3']
 
     def copy_dot_files(self):
         for item in self.dot_file_location.iterdir():
@@ -39,7 +30,19 @@ class SetupHome:
             new_dir = Path.home().joinpath(item)
             new_dir.mkdir(parents=True, exist_ok=True)
 
-    def make_dot_config(self):
+    def make_dot_fvwm(self):
+        pass
+
+
+class UserConfig:
+    def __init__(self):
+        self.dot_config_location = Path(".").joinpath("dot-config")
+        self.dot_config_dirs = ['fish', 'sakura', 'fvwm3']
+
+    def change_to_fish(self):
+        pass
+
+    def setup_dot_config(self):
         """
         cp -r dot-config/fish/* $HOME/.config/fish/.
         cp -r dot-config/sakura/* $HOME/.config/sakura/.
@@ -50,26 +53,12 @@ class SetupHome:
             new_dir = Path.home().joinpath(".config").joinpath(item)
             new_dir.mkdir(parents=True, exist_ok=True)
 
-    def make_dot_fvwm(self):
-        pass
-
     @staticmethod
     def symlink_initrc():
         # create symlink from .xinitrc to .xsession
         src = Path.home().joinpath(".xinitrc")
         target = Path.home().joinpath(".xsession")
         src.symlink_to(str(target))
-
-
-class UserConfig:
-    def __init__(self):
-        self.dot_config_location = Path(".").joinpath("dot-config")
-
-    def change_to_fish(self):
-        pass
-
-    def setup_dot_config(self):
-        pass
 
 
 if __name__ == "__main__":
