a90dd8144ed0972510b6e8e0f68357454e79d075
Ron Georgia <rong1611@gmail.com>
2023-12-30 08:42:03 -0500

    WIP

--- create_fvwm_desktop/install_fvwm3_desktop.sh
+++ create_fvwm_desktop/install_fvwm3_desktop.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #
-
+BASE_DIR=$PWD
 id | egrep "wheel|root" > /dev/null 2>&1
 if [ $? = 0 ]
 then
@@ -12,9 +12,26 @@ fi
 
 # Make sure python3.11 is installed
 sudo pkgin -y in python311
-sudo touch /etc/modules.conf
-echo "nvmm" | sudo tee -a /etc/modules.conf
-echo "compat_linux" | sudo tee -a /etc/modules.conf
+
+FILE=/etc/modules.conf
+if [ ! -f "$FILE" ]; then
+  echo "Creating ${FILE}"
+  sudo touch /etc/modules.conf
+fi
+
+grep "nvmm" $FILE
+if [ ! $? = 0 ]
+then
+    echo "Adding nvmm to ${FILE}"
+    echo "nvmm" | sudo tee -a /etc/modules.conf
+fi
+
+grep "compat_linux" $FILE
+if [ ! $? = 0 ]
+then
+    echo "Adding compat_linux to ${FILE}"
+    echo "compat_linux" | sudo tee -a /etc/modules.conf
+fi
 
 sudo ./services.py
 ./setup_home.py
@@ -27,3 +44,4 @@ echo "Updating Xresources in /etc/X11/xdm"
 sudo cp ./xdm/Xresources /etc/X11/xdm/.
 cd $HOME
 ln -s .xinitrc .xsession
+cd ${PWD}
