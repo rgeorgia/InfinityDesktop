4da592e133f61ecd2a19025d438aebd4bdd29e9b
Ron Georgia <rong1611@gmail.com>
2023-12-30 20:40:58 -0500

    WIP

--- update_torepo.sh
+++
@@ -1,7 +0,0 @@
-FVWM_HOME=$HOME/.fvwm
-REPO_FVWM_HOME=$HOME/workspace/InfinityDesktop/create_fvwm_desktop/dot-fvwm/fvwm
-CONFIG_HOME_FVWM=$HOME/.config/fvwm3
-REPO_CONFIG_FVWM=$HOME/workspace/InfinityDesktop/create_fvwm_desktop/dot-config/fvwm3
-
-cp -r ${FVWM_HOME}/* ${REPO_FVWM_HOME}/.
-cp -r ${CONFIG_HOME_FVWM}/* ${REPO_CONFIG_FVWM}/.
