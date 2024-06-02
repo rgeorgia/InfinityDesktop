f08a9b640c481bdbfff4d53d64825eaefb04396d
Ron Georgia <rong1611@gmail.com>
2024-06-02 16:20:01 -0400

    added rust plugin for vim

--- create_fvwm_desktop/misc/examples/install_govim.sh
+++ create_fvwm_desktop/misc/examples/install_govim.sh
@@ -1,8 +1,9 @@
 sudo pkgin in gh
-git clone https://github.com/fatih/vim-go.git ~/.vim/pack/plugins/start/vim-go
-git clone https://github.com/fatih/vim-go.git ~/.vim/bundle/vim-go
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+git clone https://github.com/fatih/vim-go.git ~/.vim/pack/plugins/start/vim-go
+git clone https://github.com/fatih/vim-go.git ~/.vim/bundle/vim-go
+git clone https://github.com/rust-lang/rust.vim ~/.vim/pack/plugins/start/rust.vim
 echo "Copy dot-vimrc to $HOME/.vimrc"
 cp $HOME/.vimrc $HOME/.vimrc.bak
 cp dot-vimrc $HOME/.vimrc
