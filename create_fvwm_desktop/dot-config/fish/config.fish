8f1118d30fed6724530656ff98937b98635393bd
Ron Georgia <rong1611@gmail.com>
2024-08-12 14:14:40 -0400

    Updated fish. Added awesome and rofi

--- create_fvwm_desktop/dot-config/fish/config.fish
+++ create_fvwm_desktop/dot-config/fish/config.fish
@@ -1,3 +1,9 @@
+function setXDGenv
+	set -xg XDG_CONFIG_HOME "$HOME/.config"
+	set -xg XDG_DATA_HOME "$HOME/.local/share"
+	set -xg XDG_CACHE_HOME "$HOME/.cache"
+end
+
 # Set the search path for programs.
 set -gx PATH $HOME/bin /bin /sbin /usr/bin /usr/sbin /usr/X11R7/bin /usr/pkg/bin
 set -gx PATH $PATH /usr/pkg/sbin /usr/games /usr/local/bin /usr/local/sbin
@@ -10,6 +16,5 @@ set -gx QT_QPA_PLARFORMTHEME qt5ct
 set -gx QTDIR /usr/pkg/qt5
 set -gx VISUAL $EDITOR
 
-# starship
-# starship init fish | source
-neofetch
+setXDGenv
+# neofetch
---
+++ create_fvwm_desktop/dot-config/fish/functions/xdg-config.fish
@@ -0,0 +1,4 @@
+function setXDGenv
+	set -x XDG_CONFIG_HOME "$HOME/.config"
+	set -x XDG_DATA_HOME "$HOME/.local/share"
+	set -x XDG_CACHE_HOME "$HOME/.cache"
