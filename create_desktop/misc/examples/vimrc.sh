ea7856fcbaa5bd0703da712a16ad2fc0f5f9aeaf
Ron Georgia <rong1611@gmail.com>
2025-03-07 14:18:00 -0500

    Update dot files

--- create_desktop/misc/examples/vimrc.sh
+++ create_desktop/misc/examples/vimrc.sh
@@ -1,6 +1,4 @@
 pkgin in gh
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-echo "Copy dot-vimrc to $HOME/.vimrc"
-cp $HOME/.vimrc $HOME/.vimrc.bak
-cp dot-vimrc $HOME/.vimrc
-echo "Launch vim and run :PluginInstall"
+echo "Copying ./dot-vimrc $HOME/.vimrc"
+cp ./dot-vimrc ~/.vimrc
