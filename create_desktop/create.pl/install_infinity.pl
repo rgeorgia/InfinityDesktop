f70d7d12830801093332a5377feb18d7a7f6ccc1
Ron Georgia <rong1611@gmail.com>
2026-03-19 20:48:11 -0400

    Started with checking for root

--- create_desktop/create.pl/install_infinity.pl
+++ create_desktop/create.pl/install_infinity.pl
@@ -2,12 +2,23 @@
 use strict;
 use warnings;
 
-my $username = getpwuid($<);
+sub has_root_permission {
+    my $username = getpwuid($<);
 
-my $groups_output = qx(id | grep -E "wheel|root");
-chomp($groups_output);
-my @groups = split(/\s+/, $groups_output);
+    my $groups_output = qx(id | grep -E "wheel|root");
+    chomp($groups_output);
+    my @groups = split(/\s+/, $groups_output);
+    my $search_for = "wheel";
+    my $has_wheel = grep(/$search_for/, @groups) ;
 
-print "Groups for $username: @groups\n";
+    $has_wheel;
+}
+
+
+if (! has_root_permission) {
+    die "Need sudo/doas permissions\n";
+}
+
+
 
 print "Installing\n";
